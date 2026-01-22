4228acaae5ebae55b8de1f4097d2241fd639e484
Dorian Kodelja <dorian.kodelja@se.com>
2026-01-22 09:48:39 +0100

    switch dash import to WildCard

--- packages/dash-pydantic-form/dash_pydantic_form/ids.py
+++ packages/dash-pydantic-form/dash_pydantic_form/ids.py
@@ -1,25 +1,25 @@
 from functools import partial
 
-from dash.dependencies import _Wildcard
+from dash.dependencies import WildCard
 
 
-def form_dependent_id(component: str, aio_id: str | _Wildcard, form_id: str | _Wildcard) -> dict:
+def form_dependent_id(component: str, aio_id: str | WildCard, form_id: str | WildCard) -> dict:
     """A component id to do callbacks at the document level (e.g. edit/delete)."""
     return {"component": component, "aio_id": aio_id, "form_id": form_id}
 
 
-def form_base_id(part: str, aio_id: str | _Wildcard, form_id: str | _Wildcard, parent: str | _Wildcard = ""):
+def form_base_id(part: str, aio_id: str | WildCard, form_id: str | WildCard, parent: str | WildCard = ""):
     """Form parts id."""
     return {"part": part, "aio_id": aio_id, "form_id": form_id, "parent": parent}
 
 
 def field_dependent_id(  # noqa: PLR0913
     component: str,
-    aio_id: str | _Wildcard,
-    form_id: str | _Wildcard,
-    field: str | _Wildcard,
-    parent: str | _Wildcard = "",
-    meta: str | _Wildcard = "",
+    aio_id: str | WildCard,
+    form_id: str | WildCard,
+    field: str | WildCard,
+    parent: str | WildCard = "",
+    meta: str | WildCard = "",
 ) -> dict:
     """A component id to do callbacks at the field level (e.g. in the form)."""
     return {
